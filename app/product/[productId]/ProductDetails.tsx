4a288dab8d818d9164b528c25771d296dae9bd59
Vinay Homkar <homkar2015@gmail.com>
2023-12-08 15:48:43 +0530

    Product-Details-2-SetColor

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -2,7 +2,7 @@
 
 import SetColor from "@/app/components/products/SetColor";
 import { Rating } from "@mui/material";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 interface ProductDetailsProps {
   product: any;
@@ -41,10 +41,20 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
     quantity: 1,
     price: product.price,
   });
-
+  console.log(cartProduct);
   const ProductRating =
     product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
     product.reviews.length;
+
+  const handleColorSelect = useCallback(
+    (value: SelectedImgType) => {
+      setCartProduct((prev) => {
+        return { ...prev, selectedImg: value };
+      });
+    },
+    [cartProduct.selectedImg]
+  );
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
       <div>Images</div>
@@ -69,9 +79,11 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
           {product.inStock ? "In Stock" : "Out Of Stock"}
         </div>
         <Horizontal />
-        <SetColor cartProduct={cartProduct}
-        images={product.images}
-        handleColorSelect={handleColorSelect}>
+        <SetColor
+          cartProduct={cartProduct}
+          images={product.images}
+          handleColorSelect={handleColorSelect}
+        />
         <Horizontal />
         <div>Quantity</div>
         <Horizontal />
