db6b57e7eb80f8ca9713d6b144db738e3784df24
Vinay Homkar <homkar2015@gmail.com>
2023-12-10 16:21:41 +0530

    Product-Details-5-Review

--- app/product/[productId]/ListRating.tsx
+++ app/product/[productId]/ListRating.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Heading from "@/app/components/Heading";
-import { Rating } from "@mui/material";
+import { Avatar, Rating } from "@mui/material";
 import moment from "moment";
 
 interface ListRatingProps {
@@ -16,9 +16,9 @@ const ListRating: React.FC<ListRatingProps> = ({ product }) => {
         {product.reviews &&
           product.reviews.map((review: any) => {
             return (
-              <div key={review.id} className="max-w-300px">
+              <div key={review.id} className="max-w-[300px]">
                 <div className="flex gap-2 items-center">
-                  <div>Avatar</div>
+                  <Avatar src={review?.user.image} />
                   <div className="font-semibold">{review?.user.name}</div>
                   <div className="font-light">
                     {moment(review.createdDate).fromNow()}
